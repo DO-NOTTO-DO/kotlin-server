e7181e5316386c7b67857bceb5a0cc6b736ae88e
jeonghoon <shb03323@naver.com>
2024-06-09 01:18:44 +0900

    feature: recommend / 추천 미션 전체 조회 비즈니스 로직 작성

--- application/recommend/src/main/kotlin/nottodo/recommend/controller/RecommendControllerPath.kt
+++ application/recommend/src/main/kotlin/nottodo/recommend/controller/RecommendControllerPath.kt
@@ -2,4 +2,5 @@ package nottodo.recommend.controller
 
 object RecommendControllerPath {
     const val GET_RECOMMEND_ACTIONS_BY_RECOMMEND_MISSION = "/api/v1/recommend/{recommendMissionId}/action"
+    const val GET_RECOMMEND_MISSIONS = "/api/v1/recommend/mission"
 }
