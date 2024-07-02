e9a26489150a04239845b2a20d26851a1cd52577
jeonghoon <shb03323@naver.com>
2024-07-02 22:17:46 +0900

    test: User / Test 로그인 기능 구현

--- application/user/src/main/kotlin/nottodo/user/service/social/TestLoginService.kt
+++ application/user/src/main/kotlin/nottodo/user/service/social/TestLoginService.kt
@@ -1,5 +1,7 @@
 package nottodo.user.service.social
 
+import nottodo.commonspring.exception.CustomBadRequestException
+import nottodo.persistence.rdb.domain.user.entity.SocialType
 import nottodo.user.dto.SocialInfoDto
 import nottodo.user.dto.request.LoginRequest
 import org.springframework.stereotype.Service
@@ -7,6 +9,11 @@ import org.springframework.stereotype.Service
 @Service
 class TestLoginService: SocialLoginService {
     override fun login(request: LoginRequest): SocialInfoDto {
-        TODO("Not yet implemented")
+        return SocialInfoDto(
+            email = request.email ?: throw CustomBadRequestException("TEST 로그인 시에는 email이 필요합니다."),
+            socialType = SocialType.TEST,
+            socialId = request.email,
+            name = "TEST"
+        )
     }
 }
